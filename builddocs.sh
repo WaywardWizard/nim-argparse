ed20e39cd71d54adfbc87f197172df312a5a6d93
Ben Tomlin <tomlin.ben@protonmail.com>
2026-01-26 15:01:24 +1300

    Move documentation generation to nimble task

--- builddocs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf docs/*
-# Generate indexes, then documentation. --index:only doesn't work properly.
-nim -p:src doc --project --index:on --outdir:docs src/argparse.nim > /dev/null || \
-  nim -p:src doc --project --index:on --outdir:docs src/argparse.nim
